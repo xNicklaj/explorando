dfe00591e689a241382263250d49e6663d5000e2
Simone Aronica <40305701+xNicklaj@users.noreply.github.com>
2026-01-06 16:29:45 +0100

    Images fix try 2?

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.CI ? '/explorando' : '',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   /* config options here */
 };
 
